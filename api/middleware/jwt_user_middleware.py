51a1683a3ebeaad2ca50425f73f33f87a4db211a
AlexNguyen <nguyenvietthuong0922@gmail.com>
2024-05-31 23:54:55 +0700

    remove used code

--- api/middleware/jwt_user_middleware.py
+++ api/middleware/jwt_user_middleware.py
@@ -12,24 +12,3 @@ class JWTUserMiddleware(MiddlewareMixin):
             request.user = user
         except Exception:
             request.user = AnonymousUser()
-
-        if request.method in ['POST', 'PUT', 'PATCH']:
-            if request.content_type == 'application/json':
-                try:
-                    data = json.loads(request.body)
-                    if request.user and request.user.is_authenticated:
-                        if request.method == 'POST':
-                            data['created_by'] = request.user.id
-                        else:
-                            data['updated_by'] = request.user.id
-                    request._body = json.dumps(data).encode('utf-8')
-                except json.JSONDecodeError:
-                    pass
-            elif request.content_type == 'application/x-www-form-urlencoded':
-                data = request.POST.copy()
-                if request.user and request.user.is_authenticated:
-                    if request.method == 'POST':
-                        data['created_by'] = request.user.id
-                    else:
-                        data['updated_by'] = request.user.id
-                request.POST = data
