8f859af20ef57ff407a00359bf584bdf7a687554
AlexNguyen <nguyenvietthuong0922@gmail.com>
2024-06-01 01:47:36 +0700

    apply CustomLimitOffsetPagination for users

--- api/user/user_views.py
+++ api/user/user_views.py
@@ -13,12 +13,6 @@ class UserListCreate(generics.ListCreateAPIView):
     pagination_class = CustomLimitOffsetPagination
     permission_classes = [IsAdminUser]
 
-    def list(self, request):
-        # Note the use of `get_queryset()` instead of `self.queryset`
-        queryset = self.get_queryset()
-        serializer = UserSerializer(queryset, many=True)
-        return Response(serializer.data)
-
     def post(self, request, *args, **kwargs):
         request.data["password"] = make_password(request.data["password"])
         serializer = self.get_serializer(data=request.data)
